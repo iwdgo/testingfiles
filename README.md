4c5b893246a12fd02228c43176aff638eea361c6
Constantin Konstantinidis <ConstantinKonstantinidis@gmail.com>
2019-07-18 16:31:33 +0200

    Tests added

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 [![GoDoc](https://godoc.org/github.com/iWdGo/testingfiles?status.svg)](https://godoc.org/github.com/iWdGo/testingfiles)
 [![Go Report Card](https://goreportcard.com/badge/github.com/iwdgo/testingfiles)](https://goreportcard.com/report/github.com/iwdgo/testingfiles)
 
-# Testing when large output is expected
+# Using reference files for large output
 
-This module eases the use of reference files for testing.
 A reference file (want) is useful or needed because of the size of the output or for recording purposes.
+It is useful for testing.
 
-Supported work method is to place reference files in a working directory.
+Reference files are in a working directory.
 Content is put in a file and files are compared.
 Compring a buffer (*bytes.Buffer or io.ReadCloser) is more efficient when feasible.
 
@@ -22,7 +22,19 @@ tests will panic.
 To avoid overhead, the buffer can also be compared to a file.
 If comparison fails, a got file is produced with the reminder from the difference found.
 
-## Testing
+## Testing of the module
 
-Testing requires online access. No file is saved and the first test run will fail.
-Got file can be renamed into a want file.  
+Testing requires online access for one read. Otherwise, a reference file must be available.
+No file is saved and the first test run will fail and produced the required file.
+Got file can be renamed into a want file and the second run will be successful.
+
+Benchmarking is fairly unconclusive.
+
+```
+go version go1.13beta1 windows/amd64
+pkg: github.com/iwdgo/testingfiles
+BenchmarkGetPageStringToFile-4                 1        1029407400 ns/op
+BenchmarkGetPageBufferToFile-4                 1        1108362700 ns/op
+BenchmarkGetPageBufferCompare-4                2         512716000 ns/op
+BenchmarkGetPageReadCloserCompare-4            3         545020000 ns/op
+```
--- utils_test.go
+++ utils_test.go
@@ -7,108 +7,153 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"runtime"
 	"strings"
 	"testing"
 )
 
-/*
-File operation is the most consuming. One file less means half the time.
-Buffer has a minor advantage over string.
-
-goos: windows
-goarch: amd64
-BenchmarkGetHTMLPageString-4                           1        1059377800 ns/op
-BenchmarkGetHTMLPageBuffer-4                           2         920314550 ns/op
-BenchmarkGetHTMLPageBufferNoGotFile-4                  2         513047750 ns/op
-*/
-// TODO Rename testing
+// TODO Expand into difference detection
 const (
 	techName = "Google"
 	myTech   = "MyTech"
+	wantf    = "pagewant.html"
 )
 
-// TODO Return []byte, *bytes.Buffer, ...
-func getTechHomePage() []byte {
+// Only one read on the network or filled with the existing want file
+var wantb []byte
+
+func TestMain(m *testing.M) {
 	resp, err := http.Get("https://about.google/intl/en_be/")
-	if err != nil {
-		panic(err)
+
+	OutputDir("output")
+	if err == nil {
+		defer resp.Body.Close()
+		if _, err = os.Stat(wantf); os.IsNotExist(err) {
+			// File missing, create it
+			log.Printf("creating %s file\n", wantf)
+			err = ReadCloserToFile(wantf, resp.Body)
+			if err != nil {
+				log.Fatalf("create want file failed with %v", err)
+			}
+		} else {
+			// TODO Update the file
+			log.Fatalf("%v\n", err)
+		}
+		wantb, err = ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatalf("%v\n", err)
+		}
+	} else {
+		// No network mainly... Let us fill the buffer with the file
+		// TODO Check permissions
+		f, err := os.OpenFile(wantf, os.O_RDONLY, 777)
+		if err != nil {
+			log.Fatalf("%v\n", err)
+		}
+		fs, err := os.Stat(wantf)
+		if err != nil {
+			log.Fatalf("%v\n", err)
+		}
+		wantb = make([]byte, fs.Size())
+		n, err := f.Read(wantb)
+		if err != nil {
+			log.Fatalf("%v\n", err)
+		}
+		if n != len(wantb) {
+			log.Printf("page is trucated by %d\n", len(wantb)-n)
+		}
 	}
-	defer resp.Body.Close()
-	html, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	return html
+	os.Exit(m.Run())
 }
 
-func getHTMLPage() []byte {
+// Replaces techname to get a different page
+func bytesToBuffer() *bytes.Buffer {
 	// It is assumed that replacement is case sensitive
-	return bytes.Replace(getTechHomePage(), []byte(techName), []byte(myTech), -1)
+	b := new(bytes.Buffer)
+	_, _ = b.Write(bytes.Replace(wantb, []byte(techName), []byte(myTech), -1))
+	return b
 }
 
-//  Benchmark is about the file comparison
-func GetPageStringToFile() error {
-	OutputDir("output")
-	pfileName := "pagegot.html"
-	StringToFile(pfileName, getHTMLPage())
+// Buffer is used as a string and produces a file
+// The check is using FileCompare to detect an error
+// The error is used for the test and this method by the Benchmark
+func GetPageStringToFile(name string) error {
+	// got file is identical to want file - no page update
+	StringToFile(name, wantb)
 	i, _, _, _ := runtime.Caller(0)
 	if funcname := strings.SplitAfter(filepath.Base(runtime.FuncForPC(i).Name()), "."); len(funcname) == 1 {
 		return fmt.Errorf("Func name not found")
 	} else {
-		return FileCompare(pfileName, "pagewant.html") // second element is the func name
+		return FileCompare(name, wantf) // second element is the func name
 	}
 }
 
-/* Buffer to file, iso String. Then comparing files. No real gain */
-func GetPageBufferToFile() error {
-	OutputDir("output")
-	pfileName := "pagegot.html"
-	BufferToFile(pfileName, bytes.NewBuffer(getHTMLPage()))
+// Test creation of a new file with an updated content. Error must be returned by comparison.
+func TestPageStringToFile(t *testing.T) {
+	if err := GetPageStringToFile("pagegot.html"); err != nil {
+		t.Error(err)
+	}
+}
+
+// Comparing a file to itelf must return nil
+func TestFileCompare(t *testing.T) {
+	if err := FileCompare(wantf, wantf); err != nil {
+		t.Error(err)
+	}
+}
+
+// Buffer to file, iso String. Then comparing files.
+func GetPageBufferToFile(name string) error {
+	// got file is rewritten with the updated values
+	BufferToFile(name, bytesToBuffer())
 	i, _, _, _ := runtime.Caller(0)
 	if funcname := strings.SplitAfter(filepath.Base(runtime.FuncForPC(i).Name()), "."); len(funcname) == 1 {
 		return fmt.Errorf("Func name not found")
 	} else {
-		return FileCompare(pfileName, "pagewant.html") // second element is the func name
+		return FileCompare(name, "pagewant.html") // second element is the func name
 	}
 }
 
-// No got file. Comparing buffer to want file. Got file created only if different
-func GetPageBufferCompareNoGotFile() error {
-	OutputDir("output") // for want file
-	i, _, _, _ := runtime.Caller(0)
-	if funcname := strings.SplitAfter(filepath.Base(runtime.FuncForPC(i).Name()), "."); len(funcname) == 1 {
-		return fmt.Errorf("Func name not found")
-	} else {
-		return BufferCompare(bytes.NewBuffer(getHTMLPage()), "pagewant.html")
-	}
+// Create a file from a buffer
+func TestBufferToFile(t *testing.T) {
+	b := new(bytes.Buffer)
+	b.Write(wantb)
+	BufferToFile("gotbuffer.html", b)
 }
 
-// Tests
-func TestGetHTMLPageString(t *testing.T) {
-	if err := GetPageStringToFile(); err != nil {
+func TestBufferCompare(t *testing.T) {
+	if err := BufferCompare(bytesToBuffer(), "pagewant.html"); err != nil {
 		t.Error(err)
 	}
 }
 
-func TestGetHTMLPageBuffer(t *testing.T) {
-	if err := GetPageBufferToFile(); err != nil {
+// Create a file from a ReadCloser (r.Body)
+func TestReadCloserToFile(t *testing.T) {
+	b := new(bytes.Buffer)
+	b.Write(wantb)
+	err := ReadCloserToFile("gotbuffer.html", ioutil.NopCloser(b))
+	if err != nil {
 		t.Error(err)
 	}
 }
 
-func TestGetPageBufferCompareNoGotFile(t *testing.T) {
-	if err := GetPageBufferCompareNoGotFile(); err != nil {
+func TestReadCloserCompare(t *testing.T) {
+	if err := ReadCloserCompare(ioutil.NopCloser(bytesToBuffer()), "pagewant.html"); err != nil {
 		t.Error(err)
 	}
 }
+
 
 // Benchmarks
+// File operation is the most consuming. One file less means half the time.
+// Buffer has a minor advantage over string.
 func BenchmarkGetPageStringToFile(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		if err := GetPageStringToFile(); err != nil {
+		if err := GetPageStringToFile("pagegot.html"); err != nil {
 			b.Fatal(err)
 		}
 	}
@@ -116,15 +161,43 @@ func BenchmarkGetPageStringToFile(b *testing.B) {
 
 func BenchmarkGetPageBufferToFile(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		if err := GetPageBufferToFile(); err != nil {
+		if err := GetPageBufferToFile("pagegot.html"); err != nil {
 			b.Fatal(err)
 		}
 	}
 }
 
-func BenchmarkGetPageBufferCompareNoGotFile(b *testing.B) {
+// No got file. Comparing buffer to want file. Got file created only if different
+func GetPageBufferCompare() error {
+	i, _, _, _ := runtime.Caller(0)
+	if funcname := strings.SplitAfter(filepath.Base(runtime.FuncForPC(i).Name()), "."); len(funcname) == 1 {
+		return fmt.Errorf("Func name not found")
+	} else {
+		return BufferCompare(bytesToBuffer(), "pagegot.html")
+	}
+}
+
+func BenchmarkGetPageBufferCompare(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		if err := GetPageBufferCompareNoGotFile(); err != nil {
+		if err := GetPageBufferCompare(); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+// No got file. Comparing buffer to want file. Got file created only if different
+func GetPageReadCloserCompare() error {
+	i, _, _, _ := runtime.Caller(0)
+	if funcname := strings.SplitAfter(filepath.Base(runtime.FuncForPC(i).Name()), "."); len(funcname) == 1 {
+		return fmt.Errorf("Func name not found")
+	} else {
+		return ReadCloserCompare(ioutil.NopCloser(bytesToBuffer()), "pagegot.html")
+	}
+}
+
+func BenchmarkGetPageReadCloserCompare(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		if err := GetPageReadCloserCompare(); err != nil {
 			b.Fatal(err)
 		}
 	}
